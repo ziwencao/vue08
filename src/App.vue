74e74a277bb382e954d3b6125e847bd23f4c23da
ziwenshuwu <ziwenshuwu@yeah.net>
2019-02-12 15:18:25 +0800

    ziwen music

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello ziwen
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import "common/stylus/variable"
+#app {
+  color: $color-theme
+}
+</style>
